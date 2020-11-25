7febd93aba271b5b91b99dc4a47d131d54584422
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-11-25 15:56:21 -0800

    Update favicon, manifest and robots.txt

--- client/postbuild.sh
+++ client/postbuild.sh
@@ -8,9 +8,12 @@ SERVERDIR='../../server/app'
 # Browse into './build/' directory
 cd build
 
-echo '1/2 Move build/static to server/app'
+echo '1/3 Move build/static to server/app'
 rm -rf $SERVERDIR/static
 mv static $SERVERDIR
 
-echo '2/2 move index.html to server directory'
+echo '2/3 move index.html to server directory'
 mv index.html $SERVERDIR
+
+echo '3/3 move `manifest.json`, `asset-manifest.json` and `robots.txt` to server public directory'
+mv manifest.json asset-manifest.json robots.txt $SERVERDIR/public
