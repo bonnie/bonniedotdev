bf4714deedaea1d4ddb7deaac3313097195aba7c
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-11-28 16:25:52 -0800

    Remove console.log from reducer

--- client/src/redux/reducers/courses_reducer.ts
+++ client/src/redux/reducers/courses_reducer.ts
@@ -4,7 +4,7 @@ import { actionTypes } from '../actions';
 export default function setCourses(state = [], action: CoursesActionType): CourseType[] {
   switch (action.type) {
     case actionTypes.SET_COURSES:
-      console.log('REDUCER SETS COURSES TO', action.payload); return action.payload ? action.payload : [];
+      return action.payload ? action.payload : [];
     default:
       return state;
   }
