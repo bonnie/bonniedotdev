94857cda5ad6b1b2ace6363065d926267d4a8a9d
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-12-16 11:21:44 -0800

    Update postbuild to remove images folder copy; update upload script to include index.html

--- client/postbuild.sh
+++ client/postbuild.sh
@@ -8,18 +8,15 @@ SERVERDIR='../../server/app'
 # Browse into './build/' directory
 cd build
 
-echo '1/5 Move build/static to server/app'
+echo '1/4 Move build/static to server/app'
 rm -rf $SERVERDIR/static
 mv static $SERVERDIR
 
-echo '2/5 move index.html to server directory'
+echo '2/4 move index.html to server directory'
 mv index.html $SERVERDIR
 
-echo '3/5 move `manifest.json`, `asset-manifest.json` and `robots.txt` to server public directory'
+echo '3/4 move `manifest.json`, `asset-manifest.json` and `robots.txt` to server public directory'
 mv manifest.json asset-manifest.json robots.txt $SERVERDIR/static
 
-echo '4/5 move favicon dir to server directory'
+echo '4/4 move favicon dir to server directory'
 mv favicon $SERVERDIR/static
-
-echo '5/5 move images dir to server directory'
-mv images $SERVERDIR/static
--- server/scripts/upload_build.sh
+++ server/scripts/upload_build.sh
@@ -1,10 +1,14 @@
-echo "--------------> 1/3 making tar file"
+echo "--------------> 1/4 making tar file"
 cd $PYTHONPATH/app && tar cvf static.tar static
 
-echo "--------------> 2/3 uploading tar file to aws"
+echo "--------------> 2/4 uploading tar file to aws"
 cd $PYTHONPATH/app && scp -i ~/.ssh/bdd-aws.pem static.tar ubuntu@34.212.25.188:/home/ubuntu/bonniedotdev/server/app
 
-echo "--------------> 3/3 removing tar file"
+echo "--------------> 3/4 removing tar file"
 rm $PYTHONPATH/app/static.tar
 
-echo "\nNext step: log on to aws lightsail (bdd-ssh) and \`tar xvf static.tar\` in ~/bonniedotdev/server/app"
+echo "--------------> 4/4 uploading index.html"
+cd $PYTHONPATH/app && scp -i ~/.ssh/bdd-aws.pem index.html ubuntu@34.212.25.188:/home/ubuntu/bonniedotdev/server/app/index.html.new
+
+
+echo "\nNext step: log on to aws lightsail (bdd-ssh) and update \`static\` and \`index.html\` in ~/bonniedotdev/server/app"
