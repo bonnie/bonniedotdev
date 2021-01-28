1508c443716ac00d93cd3b58ada6ab3013646a76
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-01-28 14:02:49 -0800

    Update echo statements to be more clear

--- client/postbuild.sh
+++ client/postbuild.sh
@@ -8,21 +8,21 @@ SERVERDIR='../../server/app'
 # Browse into './build/' directory
 cd build
 
-echo '1/6 Move server/app/static/images to server/app/images'
+echo '1/6 moving server/app/static/images to server/app/images'
 mv $SERVERDIR/static/images $SERVERDIR/images
 
-echo '2/6 Delete server/app/static and replace with build/static'
+echo '2/6 deleting server/app/static and replace with build/static'
 rm -rf $SERVERDIR/static
 mv static $SERVERDIR
 
-echo '3/6 Move server/app/images back into static'
+echo '3/6 moving server/app/images back into static'
 mv $SERVERDIR/images $SERVERDIR/static/images
 
-echo '4/6 move index.html to server directory'
+echo '4/6 moving index.html to server directory'
 mv index.html $SERVERDIR
 
-echo '5/6 move `manifest.json`, `asset-manifest.json` and `robots.txt` to server public directory'
+echo '5/6 moving `manifest.json`, `asset-manifest.json` and `robots.txt` to server public directory'
 mv manifest.json asset-manifest.json robots.txt $SERVERDIR/static
 
-echo '6/6 move favicon dir to server directory'
+echo '6/6 moving favicon dir to server directory'
 mv favicon $SERVERDIR/static
