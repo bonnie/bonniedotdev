3d36e58fa740c4f4ceed5d92120fe06e9e4a4ca3
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-06-15 20:52:26 -0700

    Remove talks tab

--- client/src/Pages/App/Nav/Nav.tsx
+++ client/src/Pages/App/Nav/Nav.tsx
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 export default function Nav(): ReactElement {
   const classes = useStyles();
 
-  const tabList = ['courses', 'talks', 'cheatsheets', 'about'];
+  const tabList = ['courses', 'cheatsheets', 'about'];
 
   const homeButton = (
     <Button>
